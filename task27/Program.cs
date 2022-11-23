c3e75411d51295bf67bac34384cf3a9a78f16e1d
Anton <levantonio@mail.ru>
2022-11-23 11:17:46 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,29 @@
+﻿// 27. 
+// 1. принимает на вход число
+// 2. выдает сумму цифр в чисел
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите натуральное число number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int num)
+{
+    int lastnum = 0; 
+    int result = 0;
+    
+    while (num != 0)
+    {
+        lastnum = num - num % 10;
+        result = result + (num - lastnum);
+        num = lastnum / 10;   
+    }
+    return result;    
+}
+if(number > 0)
+{
+int sumdigit = SumDigit(number);
+Console.WriteLine($"Сумма чисел в числе {number} = {sumdigit}");
+}
+else Console.WriteLine($"число {number} не натуральное");
